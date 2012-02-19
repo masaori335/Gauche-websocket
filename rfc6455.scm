69bb6ca9e9000bc658b48f0078e4434612f9d927
Masaori Koshiba <masaori335@gmail.com>
2012-02-19 22:52:51 +0900

    Bugfix: can't parse & build large frame

--- rfc6455.scm
+++ rfc6455.scm
@@ -25,6 +25,9 @@
 )
 (select-module rfc6455)
 
+(random-source-randomize! default-random-source)
+(define *max-frame-size* 16777216)
+
 (define (handshake-header key protocol)
   (define (accept key)
     (base64-encode-string (digest-string <sha1> (format "~A258EAFA5-E914-47DA-95CA-C5AB0DC85B11" key))))
@@ -58,18 +61,35 @@
     (logbit? 7 (u8vector-ref frame 1)))
 
   (define (extended-payload-length frame)
-    (cond [(= 126 (logand (u8vector-ref frame 1) #b01111111)) 4]
+    (cond [(= 126 (logand (u8vector-ref frame 1) #b01111111)) 2]
           [(= 127 (logand (u8vector-ref frame 1) #b01111111)) 8]
           [else 0]))
   
+  (define (u8vector->num frame)
+    (define (trancepose data res n)
+      (if (eq? '() data)
+          res
+          (trancepose (cdr data) (+ res (* (car data) (expt 256 n))) (+ n 1))))
+    (trancepose (reverse (u8vector->list frame)) 0 0))
+
+  (define (payload-length-ref frame)
+    (let ([extended-len (extended-payload-length frame)])
+      (cond [(= 0 extended-len) (logand (u8vector-ref frame 1) #b01111111)]
+            [(= 2 extended-len) (u8vector->num (u8vector-copy frame 2 4))]
+            [(= 8 extended-len) (u8vector->num (u8vector-copy frame 2 10))]
+            [else (error "invarid extended payload length")])))
+  
   (if (fin? frame)
-      (let ([opcode         (logand (u8vector-ref frame 0) #b00001111)]
-            [payload-length (logand (u8vector-ref frame 1) #b01111111)])
+      (let* ([opcode  (logand (u8vector-ref frame 0) #b00001111)]
+             [payload-length  (payload-length-ref frame)]
+             [extended-length (extended-payload-length frame)])
         (if (masked? frame)
-            (let ((mask    (u8vector-copy frame 2 6))
-                  (payload (u8vector-copy frame 6 (+ 6 payload-length))))
-              (xor mask payload))
-            (u8vector-copy frame 2 payload-length)))
+            (let* ([mask-position (+ 2 extended-length)]
+                   [payload-position (+ mask-position 4)]
+                   [mask    (u8vector-copy frame mask-position (+ mask-position 4))]
+                   [payload (u8vector-copy frame payload-position (+ payload-position payload-length))])
+              (xor mask payload))             
+              (u8vector-copy frame 2 payload-length)))
       (error "multi-frame")))
 
 ;; masking and unmasking are same proc just xor.
@@ -97,25 +117,23 @@
     [else (error "no such opcode")]))
 
 (define (text-frame ismask? payload)
+  (define (payload-length->u8vector len size)
+    (define (trancepose payload-vector index)
+      (u8vector-set! payload-vector (- size index 1) (ash (logand len (ash #b11111111 (* index 8))) (* index -8)))
+      (if (= index (- size 1))
+          payload-vector
+          (trancepose payload-vector (+ index 1))))
+    (trancepose (make-u8vector size) 0))
+
+  (define (mask data)
+    (xor (list->u8vector (map (lambda (x) (random-integer 128)) '(0 1 2 3))) data))
+
   (let* ([payload-length (u8vector-length payload)]
          [header-length 0]
-         [frame (make-u8vector 1024)])
-    
-    (define (payload-length->u8vector len size)
-      (let ((plvector (make-u8vector size)))
-        (define (fill index)
-          (u8vector-set! plvector (- size index) (ash (logand len (ash #b11111111 index) (* index -1))))
-          (if (> size index)
-              (fill (+ index 0))))
-        (fill 0)
-        plvector))
-    
-    (define (mask data)
-      (random-source-randomize! default-random-source)
-      (let ([key (list->u8vector (map (lambda (x) (random-integer 128)) '(0 1 2 3)))])
-        (xor key data)))
+         [frame (make-u8vector *max-frame-size*)])
 
     (u8vector-set! frame 0 #b10000001)
+
     (cond [(<= payload-length 125) (u8vector-set! frame 1 payload-length)
                                    (set! header-length 2)]
           [(<= payload-length 65535) (u8vector-set! frame 1 126)
@@ -124,8 +142,10 @@
           [else (u8vector-set! frame 1 127)
                 (u8vector-copy! frame 2 (payload-length->u8vector payload-length 8))
                 (set! header-length 10)])
+
     (if ismask? (begin (u8vector-set! frame 1 (logand (u8vector-ref frame 1) #b10000000))
                        (set! payload (mask payload))))
+
     (u8vector-copy! frame header-length payload)
     (u8vector-copy  frame 0 (+ header-length payload-length))))
 
